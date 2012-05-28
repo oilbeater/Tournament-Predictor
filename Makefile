fba1182c9188aa3d4c7e7d7bec235de69ae18574
oilbeater <liumengxinfly@gmail.com>
2012-05-28 21:48:17 +0800

    add test tool

---
+++ Makefile
@@ -0,0 +1,4 @@
+predictor: basic_predictor.cc
+			g++ basic_predictor.cc -o basic_predictor
+clean:
+			rm basic_predictor
---
+++ run.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+./basic_predictor < gcc.log
+./basic_predictor < compress.log
+./basic_predictor < crafty.log
+./basic_predictor < gzip.log
+./basic_predictor < mcf.log
+./basic_predictor < parser.log
+./basic_predictor < vpr.log
